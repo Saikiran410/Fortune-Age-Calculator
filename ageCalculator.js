0f8030583a2220723b12ba7b8251781e91405246
saikiran410 <saikiran.a410@gmail.com>
2020-06-27 12:34:34 +0530

    Age Calculator Mimim

---
+++ ageCalculator.js
@@ -0,0 +1,4 @@
+var birthYear = 1993;
+var futureYear = 2025 ;
+var age = futureYear - birthYear ;
+console.log('I will be either '+ (age - 1) + ' '+ ' Or' + ' '+ age +' '+ 'in'+' '+ futureYear);
---
+++ fortune_Teller.js
@@ -0,0 +1,7 @@
+var numChild = 2;
+var partnerName = 'Vennela Sai';
+var geographicLocation = 'Hyderabad';
+var jobTitle = 'Fullstack Developer';
+var fortune = 'You will be a' +' '+ jobTitle + ' '+'in' +' '+ geographicLocation +', and Married to ' +
+                partnerName +' '+'with' +  ' ' + numChild +' '+ 'Kids' ;
+    console.log(fortune);
